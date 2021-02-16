bcdf24127be5cadfb3397b3194463ae16ea2361d
Dzmitry Harbachou <mitya.gorbachev.2013@mail.ru>
2021-02-17 02:00:49 +0300

    WCB-5 added chat models.

--- router/index.js
+++ router/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const auth = require('./auth');
 const user = require('./user');
+const chat = require('./chat');
 
 router.use('/', auth);
 router.use('/users', user);
+router.use('/chats', chat);
 
 module.exports = router;
